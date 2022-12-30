d28ca721ee3f06ab1c845fb67afac287283f4748
lgcmotta <luiz.motta@itschmitz.com>
2022-12-30 17:00:07 -0300

    chore: change go docs

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
-// Package cqrs is an implementation of the mediator pattern to apply CQRS inspired by the C# package MediatR from Jimmy Bogard.
+// Package cqrs is an implementation of the mediator pattern to apply CQRS inspired by Jimmy Bogard's C# package: MediatR.
 //
-// cqrs is an implementation of the mediator pattern to apply CQRS inspired by the C# package MediatR from Jimmy Bogard.
+// cqrs is an implementation of the mediator pattern to apply CQRS inspired by Jimmy Bogard's C# package: MediatR.
 // # Installation
 //
 // instal the cqrs package using `go get`
