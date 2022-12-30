839a8f5d416881de3d15f2c89c1ff5444ae84138
lgcmotta <luiz.motta@itschmitz.com>
2022-12-30 16:55:09 -0300

    chore: add doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package cqrs is an implementation of the mediator pattern to apply CQRS inspired by the C# package MediatR from Jimmy Bogard.
+//
+// cqrs is an implementation of the mediator pattern to apply CQRS inspired by the C# package MediatR from Jimmy Bogard.
+// # Installation
+//
+// instal the cqrs package using `go get`
+//
+// go get -u github.com/mitz-it/golang-cqrs
+package cqrs
